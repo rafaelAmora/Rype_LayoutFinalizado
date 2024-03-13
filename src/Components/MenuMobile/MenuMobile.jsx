da2a2e13f73e968830546b6df8ab8aa7420205d4
Seu Nome1~ <seu-email@example.com>
2024-03-13 19:11:09 -0300

    menubar

--- src/Components/MenuMobile/MenuMobile.jsx
+++ src/Components/MenuMobile/MenuMobile.jsx
@@ -3,10 +3,10 @@ import "./MenuMobile.css"
 
 
 
-export default function MenuMobile({ menu, toggleMenu }) {
+export default function MenuMobile({menu}) {
+
   return (
     <>
-      {menu && <div className="menu-background" onClick={toggleMenu}></div>}
       <div className={`container-mobile ${menu ? "menu-open" : ""}`}>
         <nav className="">
           <ul className="navigator-list-Mobile">
@@ -16,21 +16,25 @@ export default function MenuMobile({ menu, toggleMenu }) {
             <Link to="/About" className="link-no-outline">
               <li className="nav_pages">A RYPE</li>
             </Link>
+
             <Link to="/Events" className="link-no-outline">
               <li className="nav_pages">EVENTOS</li>
             </Link>
             <Link to="/Trenings" className="link-no-outline">
               <li className="nav_pages">TREINOS</li>
             </Link>
+
             <Link to="/Contact" className="link-no-outline">
               <li className="nav_pages">CONTATO</li>
             </Link>
+
             <div className="create_user_Mobile">
               <Link to="/LoginPage" className="link-no-outline">
                 <li>
                   <button>ENTRAR</button>
                 </li>
               </Link>
+
               <Link to="/RegisterPage" className="link-no-outline">
                 <li>
                   <button className="button_register">REGISTRAR</button>
