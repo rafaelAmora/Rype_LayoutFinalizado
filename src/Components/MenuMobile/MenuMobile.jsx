ec7b88d4bfd05578b43186d66f1a59d51b058ee7
Seu Nome1~ <seu-email@example.com>
2024-03-13 19:06:14 -0300

    menu

--- src/Components/MenuMobile/MenuMobile.jsx
+++ src/Components/MenuMobile/MenuMobile.jsx
@@ -3,10 +3,10 @@ import "./MenuMobile.css"
 
 
 
-export default function MenuMobile({menu}) {
-
+export default function MenuMobile({ menu, toggleMenu }) {
   return (
     <>
+      {menu && <div className="menu-background" onClick={toggleMenu}></div>}
       <div className={`container-mobile ${menu ? "menu-open" : ""}`}>
         <nav className="">
           <ul className="navigator-list-Mobile">
@@ -16,25 +16,21 @@ export default function MenuMobile({menu}) {
             <Link to="/About" className="link-no-outline">
               <li className="nav_pages">A RYPE</li>
             </Link>
-
             <Link to="/Events" className="link-no-outline">
               <li className="nav_pages">EVENTOS</li>
             </Link>
             <Link to="/Trenings" className="link-no-outline">
               <li className="nav_pages">TREINOS</li>
             </Link>
-
             <Link to="/Contact" className="link-no-outline">
               <li className="nav_pages">CONTATO</li>
             </Link>
-
             <div className="create_user_Mobile">
               <Link to="/LoginPage" className="link-no-outline">
                 <li>
                   <button>ENTRAR</button>
                 </li>
               </Link>
-
               <Link to="/RegisterPage" className="link-no-outline">
                 <li>
                   <button className="button_register">REGISTRAR</button>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,53 +5,45 @@ import { useState } from "react"
 import MenuMobile from "../MenuMobile/MenuMobile"
 
 export default function NavBar() {
-  const [menu, Setmenu] = useState(false)
+  const [menu, setMenu] = useState(false)
   const handleClick = () => {
-    Setmenu(!menu)
+    setMenu(!menu)
+    document.body.classList.toggle("menu-opened", menu)
   }
-
-
-
 
   return (
     <nav className="">
-      {menu ? (
-        <MenuMobile menu={menu} />
-      ) : (
-        <ul className="navigator-list">
-          <Link to="/" className="link-no-outline">
-            <li className="nav_pages">HOME</li>
+      <MenuMobile menu={menu} /> 
+      <ul className={`navigator-list ${menu ? "hide-desktop" : ""}`}>
+      
+        <Link to="/" className="link-no-outline">
+          <li className="nav_pages">HOME</li>
+        </Link>
+        <Link to="/About" className="link-no-outline">
+          <li className="nav_pages">A RYPE</li>
+        </Link>
+        <Link to="/Events" className="link-no-outline">
+          <li className="nav_pages">EVENTOS</li>
+        </Link>
+        <Link to="/Trenings" className="link-no-outline">
+          <li className="nav_pages">TREINOS</li>
+        </Link>
+        <Link to="/Contact" className="link-no-outline">
+          <li className="nav_pages">CONTATO</li>
+        </Link>
+        <div className="create_user">
+          <Link to="/LoginPage" className="link-no-outline">
+            <li>
+              <button>ENTRAR</button>
+            </li>
           </Link>
-          <Link to="/About" className="link-no-outline">
-            <li className="nav_pages">A RYPE</li>
+          <Link to="/RegisterPage" className="link-no-outline">
+            <li>
+              <button className="button_register">REGISTRAR</button>
+            </li>
           </Link>
-
-          <Link to="/Events" className="link-no-outline">
-            <li className="nav_pages">EVENTOS</li>
-          </Link>
-          <Link to="/Trenings" className="link-no-outline">
-            <li className="nav_pages">TREINOS</li>
-          </Link>
-
-          <Link to="/Contact" className="link-no-outline">
-            <li className="nav_pages">CONTATO</li>
-          </Link>
-
-          <div className="create_user">
-            <Link to="/LoginPage" className="link-no-outline">
-              <li>
-                <button>ENTRAR</button>
-              </li>
-            </Link>
-
-            <Link to="/RegisterPage" className="link-no-outline">
-              <li>
-                <button className="button_register">REGISTRAR</button>
-              </li>
-            </Link>
-          </div>
-        </ul>
-      )}
+        </div>
+      </ul>
       <CiMenuFries className="btn-mobile" onClick={handleClick} />
     </nav>
   )
